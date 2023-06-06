f590dfa13a6636bdead1f1d256b2d50706b42be6
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-06-06 16:26:21 +0200

    palindrome word

--- js/script.js
+++ js/script.js
@@ -2,7 +2,24 @@
 Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
+*/
 
+console.log('JS OK')
+
+
+function isPalindrome(str) {
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] != str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const lookingForpalindromeWord = isPalindrome(prompt('dimmi una parola e ti diro` se e` palindroma', 'anna'))
+console.log(lookingForpalindromeWord)
+
+/*
 Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
@@ -11,4 +28,3 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
 
-console.log('JS OK')
